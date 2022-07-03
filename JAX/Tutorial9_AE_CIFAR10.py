e9687b8214843a7770ef16ef23392b77bbb20055
phlippe <phillip.lippe@googlemail.com>
2022-07-03 22:23:43 +0200

    Tutorial 9: AEs

---
+++ JAX/Tutorial9_AE_CIFAR10.py
@@ -0,0 +1,285 @@
+import tensorboard as tb
+import tensorflow as tf
+from torch.utils.tensorboard import SummaryWriter
+from torchvision.datasets import CIFAR10
+import torchvision
+import torch.utils.data as data
+import torch
+from flax.training import train_state, checkpoints
+from flax import linen as nn
+from jax import random
+import jax.numpy as jnp
+import jax
+from tqdm.auto import tqdm
+import seaborn as sns
+import matplotlib
+from matplotlib.colors import to_rgb
+import os
+import json
+import math
+import time
+import numpy as np
+from scipy import spatial
+
+import matplotlib.pyplot as plt
+from IPython.display import set_matplotlib_formats
+set_matplotlib_formats('svg', 'pdf')  # For export
+matplotlib.rcParams['lines.linewidth'] = 2.0
+sns.reset_orig()
+sns.set()
+
+import flax
+import optax
+
+
+DATASET_PATH = "../data"
+CHECKPOINT_PATH = "../saved_models/tutorial9_jax"
+timestr = time.strftime("%Y_%m_%d__%H_%M_%S")
+LOG_FILE = open(f'../logs/tutorial9_jax_{timestr}.txt', 'w')
+
+print("Device:", jax.devices()[0])
+
+
+def image_to_numpy(img):
+    img = np.array(img, dtype=np.float32)
+    if img.max() > 1:
+        img = img / 255. * 2. - 1.
+    return img
+
+
+def jax_to_torch(imgs):
+    imgs = jax.device_get(imgs)
+    imgs = torch.from_numpy(imgs.astype(np.float32))
+    imgs = imgs.permute(0, 3, 1, 2)
+    return imgs
+
+
+def numpy_collate(batch):
+    if isinstance(batch[0], np.ndarray):
+        return np.stack(batch)
+    elif isinstance(batch[0], (tuple, list)):
+        transposed = zip(*batch)
+        return [numpy_collate(samples) for samples in transposed]
+    else:
+        return np.array(batch)
+
+
+train_dataset = CIFAR10(root=DATASET_PATH, train=True,
+                        transform=image_to_numpy, download=True)
+train_set, val_set = data.random_split(
+    train_dataset, [45000, 5000], generator=torch.Generator().manual_seed(42))
+
+test_set = CIFAR10(root=DATASET_PATH, train=False,
+                   transform=image_to_numpy, download=True)
+
+train_loader = data.DataLoader(train_set, batch_size=256, shuffle=True, drop_last=True,
+                               pin_memory=True, num_workers=8, collate_fn=numpy_collate, persistent_workers=True)
+val_loader = data.DataLoader(val_set, batch_size=256, shuffle=False,
+                             drop_last=False, num_workers=4, collate_fn=numpy_collate)
+test_loader = data.DataLoader(test_set, batch_size=256, shuffle=False,
+                              drop_last=False, num_workers=4, collate_fn=numpy_collate)
+
+
+class Encoder(nn.Module):
+    c_hid: int
+    latent_dim: int
+
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Conv(features=self.c_hid, kernel_size=(
+            3, 3), strides=2)(x)  # 32x32 => 16x16
+        x = nn.gelu(x)
+        x = nn.Conv(features=self.c_hid, kernel_size=(3, 3))(x)
+        x = nn.gelu(x)
+        x = nn.Conv(features=2*self.c_hid, kernel_size=(3, 3),
+                    strides=2)(x)  # 16x16 => 8x8
+        x = nn.gelu(x)
+        x = nn.Conv(features=2*self.c_hid, kernel_size=(3, 3))(x)
+        x = nn.gelu(x)
+        x = nn.Conv(features=2*self.c_hid, kernel_size=(3, 3),
+                    strides=2)(x)  # 8x8 => 4x4
+        x = nn.gelu(x)
+        x = x.reshape(x.shape[0], -1)  # Image grid to single feature vector
+        x = nn.Dense(features=self.latent_dim)(x)
+        return x
+
+
+class Decoder(nn.Module):
+    c_out: int
+    c_hid: int
+    latent_dim: int
+
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Dense(features=2*16*self.c_hid)(x)
+        x = nn.gelu(x)
+        x = x.reshape(x.shape[0], 4, 4, -1)
+        x = nn.ConvTranspose(features=2*self.c_hid,
+                             kernel_size=(3, 3), strides=(2, 2))(x)
+        x = nn.gelu(x)
+        x = nn.Conv(features=2*self.c_hid, kernel_size=(3, 3))(x)
+        x = nn.gelu(x)
+        x = nn.ConvTranspose(features=self.c_hid,
+                             kernel_size=(3, 3), strides=(2, 2))(x)
+        x = nn.gelu(x)
+        x = nn.Conv(features=self.c_hid, kernel_size=(3, 3))(x)
+        x = nn.gelu(x)
+        x = nn.ConvTranspose(features=self.c_out,
+                             kernel_size=(3, 3), strides=(2, 2))(x)
+        x = nn.tanh(x)
+        return x
+
+
+class Autoencoder(nn.Module):
+    c_hid: int
+    latent_dim: int
+
+    def setup(self):
+        # Alternative to @nn.compact -> explicitly define modules
+        # Better for later when we want to access the encoder and decoder explicitly
+        self.encoder = Encoder(c_hid=self.c_hid, latent_dim=self.latent_dim)
+        self.decoder = Decoder(
+            c_hid=self.c_hid, latent_dim=self.latent_dim, c_out=3)
+
+    def __call__(self, x):
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        return x_hat
+
+
+def mse_recon_loss(model, params, batch):
+    imgs, _ = batch
+    recon_imgs = model.apply({'params': params}, imgs)
+    # Mean over batch, sum over pixels
+    loss = ((recon_imgs - imgs) ** 2).mean(axis=0).sum()
+    return loss
+
+
+class TrainerModule:
+
+    def __init__(self, c_hid, latent_dim, lr=1e-3, seed=42):
+        super().__init__()
+        self.c_hid = c_hid
+        self.latent_dim = latent_dim
+        self.lr = lr
+        self.seed = seed
+        # Create empty model. Note: no parameters yet
+        self.model = Autoencoder(c_hid=self.c_hid, latent_dim=self.latent_dim)
+        # Prepare logging
+        self.exmp_imgs = next(iter(val_loader))[0][:8]
+        self.log_dir = os.path.join(
+            CHECKPOINT_PATH, f'cifar10_{self.latent_dim}')
+        self.logger = SummaryWriter(log_dir=self.log_dir)
+        # Create jitted training and eval functions
+        self.create_functions()
+        # Initialize model
+        self.init_model()
+
+    def create_functions(self):
+        # Training function
+        def train_step(state, batch):
+            def loss_fn(params): return mse_recon_loss(
+                self.model, params, batch)
+            loss, grads = jax.value_and_grad(loss_fn)(
+                state.params)  # Get loss and gradients for loss
+            state = state.apply_gradients(grads=grads)  # Optimizer update step
+            return state, loss
+        self.train_step = jax.jit(train_step)
+        # Eval function
+
+        def eval_step(state, batch):
+            return mse_recon_loss(self.model, state.params, batch)
+        self.eval_step = jax.jit(eval_step)
+
+    def init_model(self):
+        # Initialize model
+        rng = jax.random.PRNGKey(self.seed)
+        rng, init_rng = jax.random.split(rng)
+        params = self.model.init(init_rng, self.exmp_imgs)['params']
+        # Initialize learning rate schedule and optimizer
+        lr_schedule = optax.warmup_cosine_decay_schedule(
+            init_value=0.0,
+            peak_value=1e-3,
+            warmup_steps=100,
+            decay_steps=500*len(train_loader),
+            end_value=1e-5
+        )
+        optimizer = optax.chain(
+            optax.clip(1.0),  # Clip gradients at 1
+            optax.adam(lr_schedule)
+        )
+        # Initialize training state
+        self.state = train_state.TrainState.create(
+            apply_fn=self.model.apply, params=params, tx=optimizer)
+
+    def train_model(self, num_epochs=500):
+        # Train model for defined number of epochs
+        best_eval = 1e6
+        for epoch_idx in tqdm(range(1, num_epochs+1)):
+            self.train_epoch(epoch=epoch_idx)
+            if epoch_idx % 10 == 0:
+                eval_loss = self.eval_model(val_loader)
+                self.logger.add_scalar(
+                    'val/loss', eval_loss, global_step=epoch_idx)
+                if eval_loss < best_eval:
+                    best_eval = eval_loss
+                    self.save_model(step=epoch_idx)
+
+    def train_epoch(self, epoch):
+        # Train model for one epoch, and log avg loss
+        losses = []
+        for batch in train_loader:
+            self.state, loss = self.train_step(self.state, batch)
+            losses.append(loss)
+        losses_np = np.stack(jax.device_get(losses))
+        avg_loss = losses_np.mean()
+        self.logger.add_scalar('train/loss', avg_loss, global_step=epoch)
+
+    def eval_model(self, data_loader):
+        # Test model on all images of a data loader and return avg loss
+        losses = []
+        batch_sizes = []
+        for batch in data_loader:
+            loss = self.eval_step(self.state, batch)
+            losses.append(loss)
+            batch_sizes.append(batch[0].shape[0])
+        losses_np = np.stack(jax.device_get(losses))
+        batch_sizes_np = np.stack(batch_sizes)
+        avg_loss = (losses_np * batch_sizes_np).sum() / batch_sizes_np.sum()
+        return avg_loss
+
+    def save_model(self, step=0):
+        # Save current model at certain training iteration
+        checkpoints.save_checkpoint(
+            ckpt_dir=self.log_dir, target=self.state.params, prefix=f'cifar10_{self.latent_dim}_', step=step)
+
+    def load_model(self, pretrained=False):
+        # Load model. We use different checkpoint for pretrained models
+        if not pretrained:
+            params = checkpoints.restore_checkpoint(
+                ckpt_dir=self.log_dir, target=self.state.params, prefix=f'cifar10_{self.latent_dim}_')
+        else:
+            params = checkpoints.restore_checkpoint(ckpt_dir=os.path.join(
+                CHECKPOINT_PATH, f'cifar10_{self.latent_dim}.ckpt'), target=self.state.params)
+        self.state = train_state.TrainState.create(
+            apply_fn=self.model.apply, params=params, tx=self.state.tx)
+
+    def checkpoint_exists(self):
+        # Check whether a pretrained model exist for this autoencoder
+        return os.path.isfile(os.path.join(CHECKPOINT_PATH, f'cifar10_{self.latent_dim}.ckpt'))
+
+
+def train_cifar(latent_dim):
+    # Create a trainer module with specified hyperparameters
+    trainer = TrainerModule(c_hid=32, latent_dim=latent_dim)
+    
+    start_time = time.time()
+    trainer.train_model(num_epochs=500)
+    train_time = time.time()
+    print(f'AE {latent_dim} latents - Full training time:',
+          time.strftime('%H:%M:%S', time.gmtime(train_time - start_time)),
+          file=LOG_FILE, flush=True)
+
+
+for latent_dim in [64, 128, 256, 384]:
+    trainer_ld, test_loss_ld = train_cifar(latent_dim)
---
+++ PyTorch/Tutorial9_AE_CIFAR10.py
@@ -0,0 +1,237 @@
+from torch.utils.tensorboard import SummaryWriter
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+import torchvision
+import torch.optim as optim
+import torch.utils.data as data
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
+from tqdm.notebook import tqdm
+import seaborn as sns
+import matplotlib
+from matplotlib.colors import to_rgb
+import os
+import json
+import time
+import math
+import numpy as np
+
+import matplotlib.pyplot as plt
+from IPython.display import set_matplotlib_formats
+set_matplotlib_formats('svg', 'pdf')  # For export
+matplotlib.rcParams['lines.linewidth'] = 2.0
+sns.reset_orig()
+sns.set()
+
+import pytorch_lightning as pl
+
+
+DATASET_PATH = "../data"
+CHECKPOINT_PATH = "../saved_models/tutorial9"
+timestr = time.strftime("%Y_%m_%d__%H_%M_%S")
+LOG_FILE = open(f'../logs/tutorial9_pytorch_{timestr}.txt', 'w')
+
+pl.seed_everything(42)
+
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+device = torch.device(
+    "cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+print("Device:", device)
+
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5,), (0.5,))])
+
+train_dataset = CIFAR10(root=DATASET_PATH, train=True,
+                        transform=transform, download=True)
+pl.seed_everything(42)
+train_set, val_set = torch.utils.data.random_split(
+    train_dataset, [45000, 5000])
+
+test_set = CIFAR10(root=DATASET_PATH, train=False,
+                   transform=transform, download=True)
+
+train_loader = data.DataLoader(
+    train_set, batch_size=256, shuffle=True, drop_last=True, pin_memory=True, num_workers=8, persistent_workers=True)
+val_loader = data.DataLoader(
+    val_set, batch_size=256, shuffle=False, drop_last=False, num_workers=4)
+test_loader = data.DataLoader(
+    test_set, batch_size=256, shuffle=False, drop_last=False, num_workers=4)
+
+
+def get_train_images(num):
+    return torch.stack([train_dataset[i][0] for i in range(num)], dim=0)
+
+
+class Encoder(nn.Module):
+
+    def __init__(self,
+                 num_input_channels: int,
+                 base_channel_size: int,
+                 latent_dim: int,
+                 act_fn: object = nn.GELU):
+        """
+        Inputs:
+            - num_input_channels : Number of input channels of the image. For CIFAR, this parameter is 3
+            - base_channel_size : Number of channels we use in the first convolutional layers. Deeper layers might use a duplicate of it.
+            - latent_dim : Dimensionality of latent representation z
+            - act_fn : Activation function used throughout the encoder network
+        """
+        super().__init__()
+        c_hid = base_channel_size
+        self.net = nn.Sequential(
+            nn.Conv2d(num_input_channels, c_hid, kernel_size=3,
+                      padding=1, stride=2),  # 32x32 => 16x16
+            act_fn(),
+            nn.Conv2d(c_hid, c_hid, kernel_size=3, padding=1),
+            act_fn(),
+            nn.Conv2d(c_hid, 2*c_hid, kernel_size=3,
+                      padding=1, stride=2),  # 16x16 => 8x8
+            act_fn(),
+            nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1),
+            act_fn(),
+            nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3,
+                      padding=1, stride=2),  # 8x8 => 4x4
+            act_fn(),
+            nn.Flatten(),  # Image grid to single feature vector
+            nn.Linear(2*16*c_hid, latent_dim)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Decoder(nn.Module):
+
+    def __init__(self,
+                 num_input_channels: int,
+                 base_channel_size: int,
+                 latent_dim: int,
+                 act_fn: object = nn.GELU):
+        """
+        Inputs:
+            - num_input_channels : Number of channels of the image to reconstruct. For CIFAR, this parameter is 3
+            - base_channel_size : Number of channels we use in the last convolutional layers. Early layers might use a duplicate of it.
+            - latent_dim : Dimensionality of latent representation z
+            - act_fn : Activation function used throughout the decoder network
+        """
+        super().__init__()
+        c_hid = base_channel_size
+        self.linear = nn.Sequential(
+            nn.Linear(latent_dim, 2*16*c_hid),
+            act_fn()
+        )
+        self.net = nn.Sequential(
+            nn.ConvTranspose2d(2*c_hid, 2*c_hid, kernel_size=3,
+                               output_padding=1, padding=1, stride=2),  # 4x4 => 8x8
+            act_fn(),
+            nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1),
+            act_fn(),
+            nn.ConvTranspose2d(2*c_hid, c_hid, kernel_size=3,
+                               output_padding=1, padding=1, stride=2),  # 8x8 => 16x16
+            act_fn(),
+            nn.Conv2d(c_hid, c_hid, kernel_size=3, padding=1),
+            act_fn(),
+            nn.ConvTranspose2d(c_hid, num_input_channels, kernel_size=3,
+                               output_padding=1, padding=1, stride=2),  # 16x16 => 32x32
+            nn.Tanh()  # The input images is scaled between -1 and 1, hence the output has to be bounded as well
+        )
+
+    def forward(self, x):
+        x = self.linear(x)
+        x = x.reshape(x.shape[0], -1, 4, 4)
+        x = self.net(x)
+        return x
+
+
+class Autoencoder(pl.LightningModule):
+
+    def __init__(self,
+                 base_channel_size: int,
+                 latent_dim: int,
+                 num_input_channels: int = 3,
+                 width: int = 32,
+                 height: int = 32):
+        super().__init__()
+        # Saving hyperparameters of autoencoder
+        self.save_hyperparameters()
+        # Creating encoder and decoder
+        self.encoder = Encoder(
+            num_input_channels, base_channel_size, latent_dim)
+        self.decoder = Decoder(
+            num_input_channels, base_channel_size, latent_dim)
+        # Example input array needed for visualizing the graph of the network
+        self.example_input_array = torch.zeros(
+            2, num_input_channels, width, height)
+
+    def forward(self, x):
+        """
+        The forward function takes in an image and returns the reconstructed image
+        """
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        return x_hat
+
+    def _get_reconstruction_loss(self, batch):
+        """
+        Given a batch of images, this function returns the reconstruction loss (MSE in our case)
+        """
+        x, _ = batch  # We do not need the labels
+        x_hat = self.forward(x)
+        loss = F.mse_loss(x, x_hat, reduction="none")
+        loss = loss.sum(dim=[1, 2, 3]).mean(dim=[0])
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        # Using a scheduler is optional but can be helpful.
+        # The scheduler reduces the LR if the validation performance hasn't improved for the last N epochs
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                         mode='min',
+                                                         factor=0.2,
+                                                         patience=20,
+                                                         min_lr=5e-5)
+        return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "val_loss"}
+
+    def training_step(self, batch, batch_idx):
+        loss = self._get_reconstruction_loss(batch)
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self._get_reconstruction_loss(batch)
+        self.log('val_loss', loss)
+
+    def test_step(self, batch, batch_idx):
+        loss = self._get_reconstruction_loss(batch)
+        self.log('test_loss', loss)
+
+
+def train_cifar(latent_dim):
+    # Create a PyTorch Lightning trainer with the generation callback
+    trainer = pl.Trainer(default_root_dir=os.path.join(CHECKPOINT_PATH, f"cifar10_{latent_dim}"),
+                         gpus=1 if str(device).startswith("cuda") else 0,
+                         max_epochs=500,
+                         callbacks=[ModelCheckpoint(save_weights_only=True),
+                                    LearningRateMonitor("epoch")],
+                         check_val_every_n_epoch=10)
+    # If True, we plot the computation graph in tensorboard
+    trainer.logger._log_graph = True
+    # Optional logging argument that we don't need
+    trainer.logger._default_hp_metric = None
+
+    model = Autoencoder(base_channel_size=32, latent_dim=latent_dim)
+    start_time = time.time()
+    trainer.fit(model, train_loader, val_loader)
+    train_time = time.time()
+    print(f'AE {latent_dim} latents - Full training time:',
+          time.strftime('%H:%M:%S', time.gmtime(train_time - start_time)),
+          file=LOG_FILE, flush=True)
+    return None, None
+
+
+for latent_dim in [64, 128, 256, 384]:
+    model_ld, result_ld = train_cifar(latent_dim)
